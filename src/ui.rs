ca8293f0e6edf61346fbeb7efd0ce0cf5e350dee
Ben Whitley <dev@purplg.com>
2023-12-08 22:56:53 -0500

    only update points when changed

--- src/ui.rs
+++ src/ui.rs
@@ -10,7 +10,7 @@ pub struct Plugin;
 impl bevy::prelude::Plugin for Plugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
-        app.add_systems(Update, update_points);
+        app.add_systems(Update, update_points.run_if(resource_changed::<Points>()));
         app.add_systems(Update, update_entity_count.run_if(on_event::<SpawnEvent>()));
     }
 }
