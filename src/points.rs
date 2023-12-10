de7e2403192710b51bb1c53534d2133b149ffd52
Ben Whitley <dev@purplg.com>
2023-12-10 12:25:58 -0500

    Reset points

--- src/points.rs
+++ src/points.rs
@@ -7,13 +7,13 @@ impl Plugin for PointsPlugin {
         app.insert_resource(Points(0));
         app.add_event::<PointEvent>();
         app.add_systems(Update, collect.run_if(on_event::<PointEvent>()));
+        app.add_systems(OnExit(crate::GameState::GameOver), reset);
     }
 }
 
 #[derive(Debug, Event)]
 pub enum PointEvent {
     Add(u32),
-    #[allow(dead_code)]
     Remove(u32),
 }
 
@@ -27,4 +27,8 @@ fn collect(mut read: EventReader<PointEvent>, mut points: ResMut<Points>) {
             PointEvent::Remove(amount) => points.0 -= amount,
         }
     }
+}
+
+fn reset(mut write: EventWriter<PointEvent>, points: Res<Points>) {
+    write.send(PointEvent::Remove(points.0));
 }
